a26200a5e06d31718555e6c908c669626bfe75eb
michalwachowicz <wachowiczmichal1@gmail.com>
2023-08-23 23:29:44 +0200

    Implement phonebook entry deletion functionality (3.4)'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const people = [
+let people = [
   {
     id: 1,
     name: "Arto Hellas",
@@ -39,6 +39,13 @@ app.get("/api/persons/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  people = people.filter((person) => person.id !== id);
+
+  response.status(204).end();
 });
 
 app.get("/info", (_, response) => {
