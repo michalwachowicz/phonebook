0eb0ee4fec4b5e73e6b72e88caf6fedd80375891
michalwachowicz <wachowiczmichal1@gmail.com>
2023-08-24 20:50:39 +0200

    Create custom validation for name and number properties

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const errorHandler = (error, _, response, next) => {
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
   }
+  if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
 
   next(error);
 };
@@ -35,15 +38,14 @@ app.get("/api/persons", (_, response) => {
   Person.find({}).then((people) => response.json(people));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const { name, number } = request.body;
-
-  if (!name || !number) {
-    return response.status(400).json({ error: "name or number missing" });
-  }
-
   const person = new Person({ name, number });
-  person.save().then((savedPerson) => response.json(savedPerson));
+
+  person
+    .save()
+    .then((savedPerson) => response.json(savedPerson))
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response, next) => {
@@ -56,12 +58,16 @@ app.put("/api/persons/:id", (request, response, next) => {
   const { name, number } = request.body;
   const person = { name, number };
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedPerson) => response.json(updatedPerson))
     .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((_) => response.status(204).end())
     .catch((error) => next(error));
--- models/person.js
+++ models/person.js
@@ -7,7 +7,21 @@ mongoose
   .then((_) => "Connected to MongoDB")
   .catch((error) => console.log("Error connecting to MongoDB", error));
 
-const personSchema = new mongoose.Schema({ name: String, number: String });
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: function (v) {
+      return /\d{2,3}-\d{4,}/.test(v);
+    },
+  },
+});
 personSchema.set("toJSON", {
   transform: (_, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
